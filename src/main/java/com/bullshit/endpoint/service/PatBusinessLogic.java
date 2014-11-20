72b2992ac7da9045d95f1fb3e03426bb0fef48ab
bigbag0815 <bigbag0815@163.com>
2014-11-20 22:01:31 +0800

    add qiniu url

--- src/main/java/com/bullshit/endpoint/service/PatBusinessLogic.java
+++ src/main/java/com/bullshit/endpoint/service/PatBusinessLogic.java
@@ -1,7 +1,9 @@
 package com.bullshit.endpoint.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import com.bullshit.endpoint.entity.Department;
 
 @Service("patLogic")
 public class PatBusinessLogic{
+	
+	private static final String PREFIX_URL = "http://ziyue991.qiniudn.com/";
 
 	@Autowired
 	private CasesExtMapper casesExtMapper;
@@ -20,11 +24,27 @@ public class PatBusinessLogic{
 	private DepartmentExtMapper departmentExtMapper;
 	
 	public List<Cases> getCasesList(String patId) throws Exception {
-		return casesExtMapper.selectByPatId(patId);
+		List<Cases> resultCaseList = new ArrayList<Cases>();
+		List<Cases> caseList = casesExtMapper.selectByPatId(patId);
+		for (Cases caseInfo : caseList) {
+			caseInfo.setPatPicUrl1(urlPathEdit(caseInfo.getPatPicUrl1(), PREFIX_URL));
+			caseInfo.setPatPicUrl2(urlPathEdit(caseInfo.getPatPicUrl2(), PREFIX_URL));
+			caseInfo.setPatPicUrl3(urlPathEdit(caseInfo.getPatPicUrl3(), PREFIX_URL));
+			
+			resultCaseList.add(caseInfo);
+		}
+		return resultCaseList;
 	}
 	
 	public List<Department> getDepartmentList(Department department) throws Exception {
 		return departmentExtMapper.selectByName(department);
 	}
 
+	private String urlPathEdit(String url, String prefixUrl){
+		if (StringUtils.isBlank(url)) {
+			return "";
+		}
+		
+		return prefixUrl + url;
+	}
 }
